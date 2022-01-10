f3985655bf7ee17a3c9eb1e75a3c7f6f243392d5
CEIBA\jose.torres <jose.torres@ceiba.com.co>
2022-01-10 17:26:29 -0500

    Test deleting things in migrations

--- microservicio-mantenimiento/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio-mantenimiento/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -6,32 +6,3 @@ create table usuario (
  primary key (id)
 );
 
-create table if not exits celda (
-    id int(11) not null auto_increment,
-    tipoVehiculo int(11) not null,
-    estadoCelda int(11) not null,
-    primary key (id)
-)
-
-create table if not exits ticket (
-    id int(11) not null auto_increment,
-    celdaId int(11) not null,
-    valor float(11) not null,
-    valorRecargaFestivos float(11) not null,
-    valorTotal float(11) not null,
-    fechaEntrada DATE not null,
-    fechaSalida DATE,
-    primary key (id),
-    INDEX (celdaId),
-    FOREIGN KEY(celdaId) REFERENCES celda(id)
-)
-
-alter table ticket
-add column cedulaUsuario varchar(100),
-add column matriculaVehiculo varchar(100),
-add column estadoVehiculo int not null,
-add column valorRecargaPorRetrasos float not null;
-
-alter table ticket
-change column estadoVehiculo estadoTicket int not null,
-add column tipoPlan int not null;
