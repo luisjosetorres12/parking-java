f7c3476c9a6f4ae5e87860d649eb63438e272ed2
CEIBA\jose.torres <jose.torres@ceiba.com.co>
2022-01-10 20:56:54 -0500

    Add

--- microservicio-mantenimiento/infraestructura/src/test/java/com/ceiba/celda/controlador/ConsultaControladorCeldaTest.java
+++ microservicio-mantenimiento/infraestructura/src/test/java/com/ceiba/celda/controlador/ConsultaControladorCeldaTest.java
@@ -15,8 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(ConsultaControladorCelda.class)
@@ -28,57 +27,11 @@ public class ConsultaControladorCeldaTest {
     private MockMvc mocMvc;
 
     @Test
-    @DisplayName("Deberia listar las celdas")
-    void deberiaListarCeldas() throws Exception {
-        mocMvc.perform(get("/celda")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
-                .andExpect(jsonPath("$[0].estadoCelda", is(0)))
-                .andExpect(jsonPath("$[0].id", is(1)));
-    }
-
-    @Test
-    @DisplayName("Deberia listar las celdas disponibles")
-    void deberiaListarCeldasDisponibles() throws Exception {
-        mocMvc.perform(get("/celda/disponibles")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
-                .andExpect(jsonPath("$[0].estadoCelda", is(1)))
-                .andExpect(jsonPath("$[0].id", is(2)));
-    }
-
-    @Test
-    @DisplayName("Deberia listar las celdas disponibles por tipo vehiculo")
-    void deberiaListarCeldasDisponiblesVehiculo() throws Exception {
-        mocMvc.perform(get("/celda/disponibles-vehiculo/0")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
-                .andExpect(jsonPath("$[0].estadoCelda", is(1)))
-                .andExpect(jsonPath("$[0].id", is(2)));
-    }
-
-    @Test
     @DisplayName("Deberia obtener una celda")
     void deberiaObtenerUnaCelda() throws Exception {
         mocMvc.perform(get("/celda/1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.tipovehiculo", is(0)))
-                .andExpect(jsonPath("$.estadoCelda", is(0)))
-                .andExpect(jsonPath("$.id", is(1)));
-    }
-
-    @Test
-    @DisplayName("Deberia obtener los detalles")
-    void deberiaObtenerDetalles() throws Exception {
-        mocMvc.perform(get("/celda/detalles")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(content().json("{'id':1, 'tipoVehiculo':0, 'estadoCelda':0}"));
     }
 }
--- microservicio-mantenimiento/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ microservicio-mantenimiento/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -1,4 +1,4 @@
-create table usuario (
+create table if not EXISTS usuario (
  id int(11) not null auto_increment,
  nombre varchar(100) not null,
  clave varchar(45) not null,
@@ -6,10 +6,26 @@ create table usuario (
  primary key (id)
 );
 
-create table celda (
+create table if not EXISTS celda (
     id int(11) not null auto_increment,
     tipoVehiculo int(11) not null,
     estadoCelda int(11) not null,
     primary key (id)
-)
+);
 
+create table if not EXISTS ticket (
+    id int(11) not null auto_increment,
+    celdaId int(11) not null,
+    valor float(11) not null,
+    valorRecargaFestivos float(11) not null,
+    valorTotal float(11) not null,
+    fechaEntrada DATE not null,
+    fechaSalida DATE,
+    cedulaUsuario varchar(1000),
+    matriculaVehiculo varchar(100),
+    tipoPlan int not null,
+    estadoTicket int not null,
+    primary key (id),
+    INDEX (celdaId),
+    FOREIGN KEY(celdaId) REFERENCES celda(id)
+);
