4d42861f4dab29ea7c78ac34a348e3a1331471dc
CEIBA\jose.torres <jose.torres@ceiba.com.co>
2022-01-10 20:02:31 -0500

    Test comando consulta

---
+++ microservicio-mantenimiento/infraestructura/src/test/java/com/ceiba/celda/controlador/ConsultaControladorCeldaTest.java
@@ -0,0 +1,86 @@
+package com.ceiba.celda.controlador;
+
+import com.ceiba.ApplicationMock;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(ConsultaControladorCelda.class)
+@ContextConfiguration(classes= ApplicationMock.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class ConsultaControladorCeldaTest {
+
+    @Autowired
+    private MockMvc mocMvc;
+
+    @Test
+    @DisplayName("Deberia listar las celdas")
+    void deberiaListarCeldas() throws Exception {
+        mocMvc.perform(get("/celda")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
+                .andExpect(jsonPath("$[0].estadoCelda", is(0)))
+                .andExpect(jsonPath("$[0].id", is(1)));
+    }
+
+    @Test
+    @DisplayName("Deberia listar las celdas disponibles")
+    void deberiaListarCeldasDisponibles() throws Exception {
+        mocMvc.perform(get("/celda/disponibles")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
+                .andExpect(jsonPath("$[0].estadoCelda", is(1)))
+                .andExpect(jsonPath("$[0].id", is(2)));
+    }
+
+    @Test
+    @DisplayName("Deberia listar las celdas disponibles por tipo vehiculo")
+    void deberiaListarCeldasDisponiblesVehiculo() throws Exception {
+        mocMvc.perform(get("/celda/disponibles-vehiculo/0")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
+                .andExpect(jsonPath("$[0].estadoCelda", is(1)))
+                .andExpect(jsonPath("$[0].id", is(2)));
+    }
+
+    @Test
+    @DisplayName("Deberia obtener una celda")
+    void deberiaObtenerUnaCelda() throws Exception {
+        mocMvc.perform(get("/celda/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].tipovehiculo", is(0)))
+                .andExpect(jsonPath("$[0].estadoCelda", is(0)))
+                .andExpect(jsonPath("$[0].id", is(1)));
+    }
+
+    @Test
+    @DisplayName("Deberia obtener los detalles")
+    void deberiaObtenerDetalles() throws Exception {
+        mocMvc.perform(get("/celda/detalles")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+    }
+}
--- microservicio-mantenimiento/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio-mantenimiento/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,2 +1,3 @@
 insert into usuario(id, nombre,clave,fecha_creacion) values(1,'test','1234',now());
 insert into celda(tipoVehiculo, estadoCelda) values(0,0);
+insert into celda(tipoVehiculo, estadoCelda) values(0,1);
